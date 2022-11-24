411a26fcec451f8cced38795528808ed42058916
Masud-Ahmed35 <masud.cse.uap@gmail.com>
2022-11-25 01:20:22 +0600

    add client reviews

--- src/Pages/HomePage/Home.js
+++ src/Pages/HomePage/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from './Banner';
+import ClientReviews from './ClientReviews/ClientReviews';
 
 const Home = () => {
     return (
         <div>
             <Banner />
+            <ClientReviews />
         </div>
     );
 };
