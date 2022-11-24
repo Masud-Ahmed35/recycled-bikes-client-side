bc4622a8722b28cc12b65af94dd905551dfda1c6
Masud-Ahmed35 <masud.cse.uap@gmail.com>
2022-11-25 01:40:03 +0600

    add review form

--- src/Pages/HomePage/Home.js
+++ src/Pages/HomePage/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from './Banner';
 import ClientReviews from './ClientReviews/ClientReviews';
+import ReviewsForm from './ReviewsForm/ReviewsForm';
 
 const Home = () => {
     return (
         <div>
             <Banner />
             <ClientReviews />
+            <ReviewsForm />
         </div>
     );
 };
