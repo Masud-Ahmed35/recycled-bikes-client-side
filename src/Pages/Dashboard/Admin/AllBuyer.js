7d48c542c3ed98d9e6200db7e3ddb0f298ee330a
Masud-Ahmed35 <masud.cse.uap@gmail.com>
2022-11-26 20:25:13 +0600

    load all buyer

--- src/Pages/Dashboard/Admin/AllBuyer.js
+++ src/Pages/Dashboard/Admin/AllBuyer.js
@@ -1,9 +1,67 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import { toast } from 'react-toastify';
 
 const AllBuyer = () => {
+    const { data: buyers = [], refetch } = useQuery({
+        queryKey: ['allBuyer'],
+        queryFn: async () => {
+            const res = await fetch(`${process.env.REACT_APP_API_URL}/allBuyer`)
+            const data = await res.json()
+            return data;
+        }
+    })
+
+    const handleDeleteProduct = buyer => {
+        const confirmation = window.confirm('Are you sure, You want to delete?');
+
+        if (confirmation) {
+            fetch(`${process.env.REACT_APP_API_URL}/allBuyer/${buyer?._id}`, {
+                method: 'DELETE',
+            })
+                .then(res => res.json())
+                .then(data => {
+                    refetch();
+                    toast.success('Deleted Successfully');
+                })
+        }
+    }
+
+
     return (
         <div>
-
+            <div className="overflow-x-auto">
+                <table className="table w-full">
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Image</th>
+                            <th>Name</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            buyers.map((buyer, i) => <tr key={buyer?._id}>
+                                <th>{i + 1}</th>
+                                <td>
+                                    <div className="avatar">
+                                        <div className="w-16 rounded-xl">
+                                            <img src={buyer?.image} alt='' />
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    <p>{buyer?.name}</p>
+                                </td>
+                                <td><button
+                                    onClick={() => handleDeleteProduct(buyer)}
+                                    className='btn btn-xs btn-error'>Delete</button></td>
+                            </tr>)
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
