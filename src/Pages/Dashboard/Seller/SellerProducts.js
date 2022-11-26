7fe19e0f655c9fe28d683f551be6ae5fc47de8a6
Masud-Ahmed35 <masud.cse.uap@gmail.com>
2022-11-26 17:07:02 +0600

    delete product by seller

--- src/Pages/Dashboard/Seller/SellerProducts.js
+++ src/Pages/Dashboard/Seller/SellerProducts.js
@@ -1,57 +1,88 @@
-import { useQuery } from '@tanstack/react-query';
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
 import { AuthContext } from '../../../Context/AuthProvider';
 
 const SellerProducts = () => {
-    const { user } = useContext(AuthContext);
+    const { user, loading, setLoading } = useContext(AuthContext);
+    const [products, setProducts] = useState([]);
+    const [refresh, setRefresh] = useState(false);
 
-    const { data: products = [] } = useQuery({
-        queryKey: ['sellerProducts'],
-        queryFn: async () => {
-            const res = await fetch(`${process.env.REACT_APP_API_URL}/sellerProducts/${user?.email}`)
-            const data = await res.json()
-            return data;
+    useEffect(() => {
+        setLoading(true)
+        fetch(`${process.env.REACT_APP_API_URL}/sellerProducts/${user?.email}`)
+            .then(res => res.json())
+            .then(data => {
+                setProducts(data)
+                setLoading(false)
+            })
+            .catch(error => {
+                console.log(error);
+                setLoading(false);
+            })
+
+    }, [user, refresh])
+
+    const handleDeleteProduct = id => {
+        const confirmation = window.confirm('Are you sure, You want to delete?');
+
+        if (confirmation) {
+            fetch(`${process.env.REACT_APP_API_URL}/products/${id}`, {
+                method: 'DELETE',
+            })
+                .then(res => res.json())
+                .then(data => {
+                    setRefresh(!refresh);
+                    toast.success('Deleted Successfully');
+                })
         }
-    })
+    }
 
     return (
         <div>
-            <div className="overflow-x-auto">
-                <table className="table w-full">
-                    <thead>
-                        <tr>
-                            <th></th>
-                            <th>Image</th>
-                            <th>Name</th>
-                            <th>Price</th>
-                            <th>Status</th>
-                            <th>Advertisement</th>
-                            <th>Action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            products.map((product, i) => <tr key={product?._id}>
-                                <th>{i + 1}</th>
-                                <td>
-                                    <div className="avatar">
-                                        <div className="w-16 rounded-xl">
-                                            <img src={product?.productImage} alt='' />
-                                        </div>
-                                    </div>
-                                </td>
-                                <td>
-                                    <p>{product?.productName}</p>
-                                </td>
-                                <td>{product?.resalePrice}</td>
-                                <td>{product?.availability}</td>
-                                <td><button className='btn btn-xs normal-case btn-outline btn-info'>Advertise</button></td>
-                                <td><button className='btn btn-xs btn-error'>Delete</button></td>
-                            </tr>)
-                        }
-                    </tbody>
-                </table>
-            </div>
+            {
+                loading ? <p>Loading...........</p>
+                    :
+                    <>
+                        <div className="overflow-x-auto">
+                            <table className="table w-full">
+                                <thead>
+                                    <tr>
+                                        <th></th>
+                                        <th>Image</th>
+                                        <th>Name</th>
+                                        <th>Price</th>
+                                        <th>Status</th>
+                                        <th>Advertisement</th>
+                                        <th>Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {
+                                        products.map((product, i) => <tr key={product?._id}>
+                                            <th>{i + 1}</th>
+                                            <td>
+                                                <div className="avatar">
+                                                    <div className="w-16 rounded-xl">
+                                                        <img src={product?.productImage} alt='' />
+                                                    </div>
+                                                </div>
+                                            </td>
+                                            <td>
+                                                <p>{product?.productName}</p>
+                                            </td>
+                                            <td>{product?.resalePrice}</td>
+                                            <td>{product?.availability}</td>
+                                            <td><button className='btn btn-xs normal-case btn-outline btn-info'>Advertise</button></td>
+                                            <td><button
+                                                onClick={() => handleDeleteProduct(product?._id)}
+                                                className='btn btn-xs btn-error'>Delete</button></td>
+                                        </tr>)
+                                    }
+                                </tbody>
+                            </table>
+                        </div>
+                    </>
+            }
         </div>
     );
 };
